cc0e23c1da8896b9067e1c1894e60f05d881bbfa
PouyaniArmin <pouyaniarmin@gmail.com>
2023-06-24 19:01:40 +0330

    index

--- public/index.php
+++ public/index.php
@@ -1,14 +1,15 @@
 <?php
 
 use Conterollers\ContactController;
+use Conterollers\HomeController;
 use Conterollers\ProductConterller;
 use Core\Application;
+use Dotenv\Dotenv;
 
 require_once __DIR__."/../vendor/autoload.php";
-
 $app=new Application(dirname(__DIR__));
-$app->router->get('/','home');
 
+$app->router->get('/',[HomeController::class,'index']);
 $app->router->get('/contact',[ContactController::class,'index']);
 $app->router->post('/contact',[ContactController::class,'index']);
 $app->router->get('/product',[ProductConterller::class,'index']);
