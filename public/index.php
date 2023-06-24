108f8e02b19c80f6883503f43734e8c01afad283
PouyaniArmin <pouyaniarmin@gmail.com>
2023-06-24 10:30:04 +0330

    index

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
 
+use Conterollers\ContactController;
 use Conterollers\ProductConterller;
 use Core\Application;
 
@@ -8,10 +9,8 @@ require_once __DIR__."/../vendor/autoload.php";
 $app=new Application(dirname(__DIR__));
 $app->router->get('/','home');
 
-$app->router->get('/contact',function(){
-    return Application::$app->router->renderView('contactus');
-});
-
+$app->router->get('/contact',[ContactController::class,'index']);
+$app->router->post('/contact',[ContactController::class,'index']);
 $app->router->get('/product',[ProductConterller::class,'index']);
 
 $app->run();
